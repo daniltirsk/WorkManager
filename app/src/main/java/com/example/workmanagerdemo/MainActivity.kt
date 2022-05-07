5d00f75e3768e193f1d8522ce62aab242b96d627
danil <daniltirskk@gmail.com>
2022-05-07 23:45:34 +0800

    completed todos

--- app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerdemo/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.workmanagerdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
+import androidx.work.workDataOf
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val myData1: Data = workDataOf("CITY" to "Svirsk")
+        val myData2: Data = workDataOf("CITY" to "Irkutsk")
+        val myData3: Data = workDataOf("CITY" to "Angarsk")
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val workRequest = OneTimeWorkRequest.Builder(WeatherWorker::class.java).build()
-        WorkManager.getInstance(this).enqueue(workRequest)
-
-        // TODO: реализовать запрос погоды
-        // TODO: какие параметры позволяет задать WorkManager (макс число одновр заданий)
-        // TODO: реализовать выполнение заданий по очереди
-
+        val workRequest1 = OneTimeWorkRequest.Builder(WeatherWorker::class.java).setInputData(myData1).build()
+        val workRequest2 = OneTimeWorkRequest.Builder(WeatherWorker::class.java).setInputData(myData2).build()
+        val workRequest3 = OneTimeWorkRequest.Builder(WeatherWorker::class.java).setInputData(myData3).build()
+        WorkManager.getInstance(this).beginWith(workRequest1)
+            .then(workRequest2)
+            .then(workRequest3).enqueue()
     }
 }
--- app/src/main/java/com/example/workmanagerdemo/WeatherWorker.kt
+++ app/src/main/java/com/example/workmanagerdemo/WeatherWorker.kt
@@ -2,14 +2,42 @@ package com.example.workmanagerdemo
 
 import android.content.Context
 import android.util.Log
+import android.content.res.Resources
+import android.provider.Settings.Global.getString
+import android.provider.Settings.System.getString
+import androidx.core.content.res.TypedArrayUtils.getString
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.io.InputStream
+import java.net.URL
+import java.util.*
+
+const val KEY_CITY_ARG = "CITY"
+
+const val KEY_RESULT = "result"
+
 
 class WeatherWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams)
 {
     override fun doWork(): Result {
-        Log.d("mytag", "work success")
+        val city = inputData.getString(KEY_CITY_ARG)
+        if (city != null) {
+            Log.d("mytag", city)
+        }
+        val API_KEY = "d6843ab8ee963f5d372296dfff62aed7"
+        val weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric";
+        val stream: InputStream
+        try {
+            stream = URL(weatherURL).getContent() as InputStream
+        } catch (e: Exception){
+            Log.d("mytag", e.toString())
+            return Result.failure()
+        }
+
+        val data = Scanner(stream).nextLine()
+        Log.d("mytag", data)
+
         return Result.success()
     }
 }
